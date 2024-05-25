e3ec143e24729560d8911077ea428d274c8c9584
Eli Burch <eli.burch@burchbytes.com>
2024-05-25 09:30:28 -0700

    Fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ fi
 
 if [ ! -f "/home/github/.config/systemd/user/workouttracker.service" ];
 then
-  ln -s "/home/github/.config/systemd/user/workouttracker.service" "etc/systemd/workouttracker"
+  ln -s "etc/systemd/workouttracker.service" "/home/github/.config/systemd/user/workouttracker.service"
 fi
 
 systemctl --user restart workouttracker
